2d5659541d69247dd5c674b114b9c6977adfa1b1
rbapat <rorobapat@gmail.com>
2016-01-10 13:32:18 -0500

    Update Robot.java

--- Code/Java/Six Wheel Drive/src/org/usfirst/frc/team263/robot/Robot.java
+++ Code/Java/Six Wheel Drive/src/org/usfirst/frc/team263/robot/Robot.java
@@ -1,15 +1,25 @@
 package org.usfirst.frc.team263.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends SampleRobot {
     Joystick DrivePad;
     SixWD Drive;
-    
+    CameraServer server1;
+    CameraServer server2;
     public Robot() {
         DrivePad = new Joystick(0);
         Drive = new SixWD();
+        
+        server1 = CameraServer.getInstance();
+        server1.setQuality(50);
+        server1.startAutomaticCapture("cam0");
+        
+        server2 = CameraServer.getInstance();
+        server2.setQuality(50);
+        server2.startAutomaticCapture("cam2");
     }
 
  
